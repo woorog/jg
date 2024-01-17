153f5f1f1a133fa1ae0c3d52ed492827834befb5
woorog <nohanwoo@gmail.com>
2024-01-17 19:27:25 +0900

    hihi

--- test.py
+++ test.py
@@ -1,12 +1,28 @@
-import heapq
-import sys
-from collections import deque
+
 
 # a = list(map(int, sys.stdin.readline().split()))
 # a = [1, 2, 3, 4, 5]
 #
 
-
+#문자열 내장 함수 코딩테스트를 위한 Python 정리
+#▶ lower ▶ upper ▶ find ▶ count
+#
+#리스트 내장함수
+# append
+# insert
+# a.insert(i, v) : i번 인덱스에 v 추가
+#pop
+# a.pop() : 마지막 원소 꺼내기 (꺼낸 값 반환)
+# a.pop(i) : i번 인덱스 값 꺼내기 (꺼낸 값 반환)
+# remove
+#  a.remove(v) : v 값을 찾아서 제거
+# index
+# a.index(v) : v가 리스트의 몇 번째 index에 있는지 반환
+#ord( ) / chr( )
+#ord()는 문자열을 아스키코드(숫자)로 반환하는 함수이고, chr()은 아스키코드를 문자열로 반환하는 함수!
+#isalpha( ) / isalnum( )
+#isalpha()는 문자열이면 True, 아니면 False를 리턴하고,
+#isalnum()은 숫자면 True, 아니면 False를 리턴함
 
 # 여러 문장.
 # n = input()
@@ -22,30 +38,32 @@ from collections import deque
 # temp = sys.stdin.readline().split() 종류가 다른것도
 #direction_changes = dict(sys.stdin.readline().split() for _ in range(l))
 # 키 값 딕셔너러리로 받는법.
-
+# product()
+# p, q, … [repeat=1]
+# 데카르트 곱(cartesian product), 중첩된 for 루프와 동등합니다
+# permutations()
+# p[, r]
+# r-길이 튜플들, 모든 가능한 순서, 반복되는 요소 없음
+# combinations()
+# p, r
+# r-길이 튜플들, 정렬된 순서, 반복되는 요소 없음
+#
+#heapq 사용법  heapq.heappop(heap) heappush(heap,12)
 from heapq import *
-def max_people(n, positions, d):
+import sys
+import heapq
 
-    max_people = 0
+num = int(sys.stdin.readline())
+listnum = [int(sys.stdin.readline().strip()) for _ in range(num)]
+heap=[]
 
+for i in range(num):
+    if listnum[i]==0:
+        if len(heap)==0:
+            print(0)
+        else:
+            smallest = heapq.heappop(heap)
+            print(smallest)
+    else:
+        heappush(heap,listnum[i])
 
-    for i in range(n):
-        count = 0
-        start_point = positions[i][0]
-        end_point = start_point + d
-
-
-        for hi, oi in positions:
-            if start_point <= hi <= end_point and start_point <= oi <= end_point:
-                count += 1
-
-        max_people = max(max_people, count)
-
-    return max_people
-
-n = int(sys.stdin.readline())
-positions = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
-d = int(sys.stdin.readline())
-
-
-print(max_people(n, positions, d))
