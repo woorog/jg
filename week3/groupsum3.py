affd59423109106b1cb71e966ac303665813a8a9
woorog <nohanwoo@gmail.com>
2024-01-31 10:53:03 +0900

    1-30

--- week3/groupsum3.py
+++ week3/groupsum3.py
@@ -7,18 +7,13 @@ ans = numlist[0]
 dp = numlist[0]
 
 for i in range(1, num):
-
     now = numlist[i]
-
-    if now > ans: # 현재가 최대보다 크면 처음부터 넣어주기
-        ans = now
-
     dp+= now  #더해서 넣기
-
-    if ans < dp: #답이랑 지금넣은거 비교해서 크면 답으로 설정
-        ans = dp
 
     if dp < now:  # 현재랑 과거랑 비교해서 크면 dp에 넣어주기
         dp= now
+    if ans < dp: #답이랑 지금넣은거 비교해서 크면 답으로 설정
+        ans = dp
+
 
 print(ans)
